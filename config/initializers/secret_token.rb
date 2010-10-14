ec088a161891120672bf23d709477672472b15d1
nate.cui <cuiyingsan@gmail.com>
2010-10-14 14:20:18 +0800

    sprint 1-5.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lbsc2::Application.config.secret_token = 'dc825992eb5a64d9a9d0749c552fccb8eaede75c7e1a58b0e28e09c7122031bc4aec8abbbb91e68a15c59e0d4cd23fe63653ad38d5e19a50a0fe46391b0f5c83'
